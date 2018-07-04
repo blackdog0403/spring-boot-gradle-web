4c6f3bf58b325917b1d28b72cf2d5ef59ce1572c
Kwangyoung Allen Kim <kwangyoung49.kim@samsung.com>
2018-07-04 11:14:45 +0900

    modify docker file path and build path

--- docker-build.sh
+++ docker-build.sh
@@ -4,9 +4,7 @@ set -e
 
 ./gradlew clean build -PprojVersion=0.0.1-RELEASE
 
-cp build/libs/spring-boot-sample-web-ui-0.0.1-RELEASE.war docker-webapp/spring-boot-sample-web-ui-0.0.1-RELEASE.war
+docker build --no-cache  -f Dockerfile-webapp -t springbootsampleapp:0.0.1-RELEASE .
+docker build --no-cache  -f Dockerfile-nginx  -t nginxreverseproxy:0.0.1-RELEASE .
 
-docker build --no-cache -t springbootsampleapp:0.0.1-RELEASE docker-webapp
-docker build --no-cache -t nginxreverseproxy:0.0.1-RELEASE docker-nginx
-
-rm docker-webapp/spring-boot-sample-web-ui-0.0.1-RELEASE.war
+./gradlew clean

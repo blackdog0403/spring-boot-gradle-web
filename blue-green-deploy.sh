f78de25e5e495c914eed1a47656155bc6c1afc07
Kwangyoung Allen Kim <kwangyoung49.kim@samsung.com>
2018-07-08 02:49:00 +0900

    modify blue-green-deploy.sh and yamls

--- blue-green-deploy.sh
+++ blue-green-deploy.sh
@@ -6,8 +6,13 @@ echo "Blue-Green Deployment started..."
 
 echo "Route to secondary"
 cat conf/nginx/nginx.conf_sec > conf/nginx/nginx.conf
-docker exec -it $(docker ps | awk '{print $NF}' | grep nginx) nginx -s reload
-sleep 5
+
+CID=$(docker ps | awk '{print $NF}' | grep nginx)
+for cid in $CID; do
+	docker exec -it $cid nginx -s reload
+done
+
+sleep 10
 
 echo "Deploy new application to primary"
 docker stack deploy -c docker-compose.blue.yml --resolve-image=changed mydemoapp
@@ -15,8 +20,13 @@ sleep 60
 
 echo "Route to pri"
 cat conf/nginx/nginx.conf_pri > conf/nginx/nginx.conf
-docker exec -it $(docker ps | awk '{print $NF}' | grep nginx) nginx -s reload
-sleep 5
+
+CID=$(docker ps | awk '{print $NF}' | grep nginx)
+for cid in $CID; do
+	docker exec -it $cid nginx -s reload
+done
+
+sleep 10
 
 echo "Deploy new application to secondary"
 docker stack deploy -c docker-compose.green.yml --resolve-image=changed mydemoapp
@@ -24,6 +34,10 @@ sleep 60
 
 echo "Route round robin"
 cat conf/nginx/nginx.conf_rr > conf/nginx/nginx.conf
-docker exec -it $(docker ps | awk '{print $NF}' | grep nginx) nginx -s reload
+
+CID=$(docker ps | awk '{print $NF}' | grep nginx)
+for cid in $CID; do
+	docker exec -it $cid nginx -s reload
+done
 
 echo "New application deployment finished"
